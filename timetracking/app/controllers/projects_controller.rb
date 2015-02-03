e0bb8903419bce16c47d10e043ed11efac7e9408
Mingos <dcividanes91@gmail.com>
2015-02-03 15:02:32 +0100

    Time tracking added to the project view

--- timetracking/app/controllers/projects_controller.rb
+++ timetracking/app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
 	def index
 		@list = Project.last_created_projects(10)
+		@worked = Project.total_hours_in_month
 		if @list.empty?
 			render 'no_projects', layout: 'no_projects'
 		end
@@ -8,6 +9,7 @@ class ProjectsController < ApplicationController
 	def show
 		begin
 			@project = Project.find params[:id]
+			@worked = @project.total_hours_in_month
 		rescue ActiveRecord::RecordNotFound
 			render 'no_projects_found'
 		end
--- timetracking/app/models/project.rb
+++ timetracking/app/models/project.rb
@@ -9,4 +9,17 @@ class Project < ActiveRecord::Base
 	def self.last_created_projects number
 		order("created_at DESC").limit(number)
 	end
+
+	def total_hours_in_month
+		from = Date.new(Date.today.year,Date.today.month,1)
+		to = Date.new(Date.today.year,Date.today.month, -1)
+		total = 0
+		minutes = 0
+		entries.where(date: from..to).each do |entry|
+			total += entry.hours
+			minutes += entry.minutes
+		end
+		total += minutes / 60
+		total.to_s + " h " + (minutes % 60).to_s + " min"
+	end
 end
